e5c17df90f6f29a76f443e0176929c57e58fa184
Shivang Chikani <shivangchikani1@gmail.com>
2022-11-01 11:52:27 +0530

    Update install.py

--- install.py
+++ install.py
@@ -14,9 +14,19 @@ if len(args) > 2:
 else:
     _board_name_ = "pyboard"
 
-rshell_mkdir = f"mkdir /{_board_name_}/lib; mkdir /{_board_name_}/lib/ushell; mkdir /{_board_name_}/lib/jsonDB; mkdir /{_board_name_}/lib/brain_lang"
-rshell_cp_files = f"cp -r ./ushell/*.py /{_board_name_}/lib/ushell; cp -r ./jsonDB/*.py /{_board_name_}/lib/jsonDB; cp -r ./brain_lang/*.py /{_board_name_}/lib/brain_lang; cp ush.py /{_board_name_}"
-rshell_repl_cmds = f"repl ~ from ush import *"
-rshell_all_cmds = f"{rshell_mkdir}; {rshell_cp_files}; {rshell_repl_cmds}"
+def submodules_update():
+    os.system("git submodule update --init --recursive")
 
-os.system(f'rshell --buffer-size 8024 -p {_port_} "{rshell_all_cmds}"')
+
+def rshell_upload_files():
+    os.system("pip3 install rshell")
+    rshell_mkdir = f"mkdir /{_board_name_}/lib; mkdir /{_board_name_}/lib/ushell; mkdir /{_board_name_}/lib/jsonDB; mkdir /{_board_name_}/lib/brain_lang"
+    rshell_cp_files = f"cp -r ./ushell/*.py /{_board_name_}/lib/ushell; cp -r ./jsonDB/*.py /{_board_name_}/lib/jsonDB; cp -r ./brain_lang/*.py /{_board_name_}/lib/brain_lang; cp ush.py /{_board_name_}"
+    rshell_repl_cmds = f"repl ~ from ush import *"
+    rshell_all_cmds = f"{rshell_mkdir}; {rshell_cp_files}; {rshell_repl_cmds}"
+    os.system(f'rshell --buffer-size 8024 -p {_port_} "{rshell_all_cmds}"')
+
+
+
+submodules_update()
+rshell_upload_files()
